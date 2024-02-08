3a0e9489338beccbb51f29f501bad3c0663cd3fd
aelim <cloim09@gmail.com>
2024-02-08 11:39:48 +0900

    add Exception service -> Exception test
    Repository
    add find MemberId

--- src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
+++ src/main/java/com/fastcampus/programming/dmaker/service/DMakerService.java
@@ -2,6 +2,8 @@ package com.fastcampus.programming.dmaker.service;
 
 import com.fastcampus.programming.dmaker.dto.CreateDeveloper;
 import com.fastcampus.programming.dmaker.entity.Developer;
+import com.fastcampus.programming.dmaker.exception.DMakerErrorCode;
+import com.fastcampus.programming.dmaker.exception.DMakerException;
 import com.fastcampus.programming.dmaker.repository.DeveloperRepository;
 import com.fastcampus.programming.dmaker.type.DeveloperLevel;
 import com.fastcampus.programming.dmaker.type.DeveloperSkillType;
@@ -10,14 +12,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static com.fastcampus.programming.dmaker.exception.DMakerErrorCode.*;
+
 @Service
 @RequiredArgsConstructor
 public class DMakerService {
     private final DeveloperRepository developerRepository;
 
     @Transactional
-    public void  createDeveloper(CreateDeveloper.@Valid Request request){
-        Developer developer =Developer.builder()
+    public void createDeveloper(CreateDeveloper.@Valid Request request) {
+        //business logic start
+        Developer developer = Developer.builder()
                 .developerLevel(DeveloperLevel.JUNIOR)
                 .developerSkillType(DeveloperSkillType.FRONT_END)
                 .experienceYears(2)
@@ -26,6 +33,35 @@ public class DMakerService {
                 .build();
 
         developerRepository.save(developer);
+    }
+
+    private void validateCreateDeveloperRequest(CreateDeveloper.Request request) {
+        //business validation
+        DeveloperLevel developerLevel = request.getDeveloperLevel();
+        Integer experienceYears = request.getExperienceYears();
+        if (developerLevel == DeveloperLevel.SENIOR
+                && experienceYears < 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED); //에외처리 따로 만든 후 메시지
+            // RuntimeException("SENIOR need 10 years experience."); 예외처리를 따로 만들어 주기 전 메시지
+        }
+        if (developerLevel == DeveloperLevel.JUNGNIOR
+                && experienceYears < 4 || experienceYears > 10) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+        if (developerLevel == DeveloperLevel.JUNIOR && experienceYears > 4) {
+            throw new DMakerException(LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+        }
+
+//        Optional<Developer> developer =developerRepository.findByMemberId(request.getMemberId());
+//        if(developer.isPresent())
+//            throw new DMakerException(DUPLICATED_MEMBER_ID);
+        //  위에 코드와 동일한거고, 자바8버전 이상부터는 아래 코드 사용하는데 developer가 에러나네... 이유는 모르겠군..ㅜㅜ
+        // 이유는 isPresent에서 ifPersent인데 자동 완성이 is로 되어버림 ><
+        developerRepository.findByMemberId(request.getMemberId())
+                .ifPresent((developer -> {
+                    throw new DMakerException(DUPLICATED_MEMBER_ID);
+                }));
+
     }
 
 }
